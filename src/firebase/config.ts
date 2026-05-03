44741d422ce0a000345bb00cfbc68c6004673ffc
Bimbel Level Up <levelupbimbel5@gmail.com>
2026-05-03 18:23:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5970366270-39ecb",
+  "appId": "1:342107627585:web:1a8aa7bc0f7467bb8926c0",
+  "apiKey": "AIzaSyAa3SKtP3wZJCDac4Sf9ASaLACjYHTlAV0",
+  "authDomain": "studio-5970366270-39ecb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "342107627585"
+};
